8515022194c89a8a182dfe9e7e30c3fed68f0da8
Luca Crippa <luca.crippa88@gmail.com>
2018-12-11 14:18:18 +0100

    Update pageClick.js

--- js/pageClick.js
+++ js/pageClick.js
@@ -1,1 +1,25 @@
-
+/*
+ * Function: pageClick ©
+ * Requires:
+  - page number
+  - selected category
+  - action
+ * Returns: redirects to clicked page
+ * Author: Luca Crippa - luca.crippa88@gmail.com
+ * Date: February 2018
+ * Comments: called inside generateList()
+ */
+function pageClick(page, category, toget){
+  $(".page-click").click(function(){
+    var clicked = $(this).children().text();
+    if(toget == "onlypage") {
+      window.location.href = path+"/"+page+"?pagina="+clicked;
+    }
+    if(toget == "onlycategory") {
+      window.location.href = path+"/"+page+"?categoria="+category;
+    }
+    if(toget == "all") {
+      window.location.href = path+"/"+page+"?categoria="+category+"&pagina="+clicked;
+    }
+  });
+}
