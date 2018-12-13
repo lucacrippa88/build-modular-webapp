33a89edf961b861749f67057e2e73bcf546990e4
Luca Crippa <luca.crippa88@gmail.com>
2018-12-13 10:09:55 +0100

    Create RetrievePost.php

---
+++ api/RetrievePost.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * API: RetrievePost ©
+ * Returns: the seledted post values from the DB
+ * Author: Luca Crippa - luca.crippa88@gmail.com
+ * Date: December 2017
+ */
+
+  // Perform connection to DB (set table name)
+  $tableName = "Contents";
+  @include("../admincfg/ConnectDB.php");
+
+  // Parameters
+  //--------------------------------------------------------------------------------------
+
+  $url = $_POST['ur'];
+
+  $sql = "SELECT * FROM $tableName WHERE URL='$url' AND PAGE='' AND PUBLIC='true'";
+
+
+  // Allow special characters (i.e. €)
+  mysqli_set_charset($con, "utf8");
+
+  //--------------------------------------------------------------------------------------
+  $result = mysqli_query($con, $sql);
+  $numrows = mysqli_num_rows($result);
+
+    if($numrows > 0){
+      while($row = mysqli_fetch_assoc($result)){
+        // Prepare response, close connection and send response to front-end
+        $array['Response'][] = array(
+          'id' => $row['ID'],
+          'posted' => $row['POSTED'],
+          'updated' => $row['UPDATED'],
+          'url' => $row['URL'],
+          // 'page' => $row['PAGE'],
+          // 'check1' => $row['CHECK1'],
+          'check2' => $row['CHECK2'],
+          'check3' => $row['CHECK3'],
+          'select1' => $row['SELECT1'],
+          'select2' => $row['SELECT2'],
+          'chips1' => $row['CHIPS1'],
+          'datetimes' => $row['DATETIMES'],
+          'maincontent' => $row['MAINCONTENT'],
+          'public' => $row['PUBLIC']
+        );
+      }
+    }
+
+    mysqli_close($con);
+    echo json_encode($array); // Returns confirmation or query error
+
+?>
