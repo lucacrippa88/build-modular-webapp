fd731635732cf28f1e69252391f5eb14ddb5f657
Luca Crippa <luca.crippa88@gmail.com>
2018-12-17 09:54:13 +0100

    Create errore.php

---
+++ login/errore.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head></head>
+<!-- JQuery -->
+<script type="text/javascript" src="js/genericFunctions.js"></script>
+<script type="text/javascript" src="login/login.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@7.29.2/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+
+<script>
+  createHead();
+  errorPage();
+</script>
+
+<body>
+
+<?php include("lib/nav.html") ?>
+
+</body>
+
+</html>
