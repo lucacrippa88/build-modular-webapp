000681be457c30d72ba15e07330db2246ac421d9
Luca Crippa <luca.crippa88@gmail.com>
2018-12-17 09:53:37 +0100

    Create logout.php

---
+++ login/logout.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="it">
+
+<head>
+  <meta charset='utf-8'>
+  <meta name='description' class='put-head-desc-here' content=''>
+  <meta name='keywords' class='put-head-keys-here' content=''>
+</head>
+<script type="text/javascript" src="js/genericFunctions.js"></script>
+<script type="text/javascript" src="js/functions.js"></script>
+<script type="text/javascript" src="login/login.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@7.29.2/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+
+<body>
+
+<?php include("lib/nav.html") ?>
+
+</body>
+
+<script>
+createHead();
+requestLogout();
+</script>
+
+</html>
